e7b863264a021fa6256ed57bdde1ace08349671c
nguyen-thanh-luann <imthanhluan@gmail.com>
2022-07-27 23:34:01 +0700

    create header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Header from './components/header/Header'
+import LoadingBox from './components/LoadingBox'
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
     </BrowserRouter>
   )
 }
